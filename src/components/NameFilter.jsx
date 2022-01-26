41633e89c1bebb8f698f4a05482b89f05861b2e3
Julia Barcelos <julaai@gmail.com>
2022-01-26 12:35:20 -0300

    remove function to submit form and form, transform it in div with a class filters.

--- src/components/NameFilter.jsx
+++ src/components/NameFilter.jsx
@@ -2,26 +2,20 @@ import React, { useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 function NameFilter() {
-  const { setNameFilter, setDataName, data } = useContext(StarWarsContext);
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const filterPlanetName = data.filter((item) => item.name.includes(event));
-    setDataName(filterPlanetName);
-  }
+  const { handleFilterName, filters } = useContext(StarWarsContext);
 
   return (
-    <form onSubmit={ handleSubmit }>
-      <label htmlFor="nameFilter">
-        <input
-          data-testid="name-filter"
-          name="nameFilter"
-          placeholder="Filtro por nome"
-          id="nameFilter"
-          onChange={ (event) => setNameFilter(event.target.value) }
-        />
-      </label>
-    </form>
+    <div className="filters">
+      {/* onSubmit={ handleSubmit } */ }
+      <input
+        type="text"
+        data-testid="name-filter"
+        placeholder="Filtre por nome"
+        id="nameFilter"
+        value={ filters.byName }
+        onChange={ handleFilterName }
+      />
+    </div>
   );
 }
 
