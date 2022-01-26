3707e3c77438338217c80b01a9b0b748d3e25d36
Julia Barcelos <julaai@gmail.com>
2022-01-25 23:40:58 -0300

    reorganized table component, made a conditional to render with or off filterName.

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 function Table() {
-  const { data } = useContext(StarWarsContext);
+  const { data, nameFilter, dataName } = useContext(StarWarsContext);
   const [tableTitle, setTableTitle] = useState([]);
 
   useEffect(() => {
@@ -12,6 +12,26 @@ function Table() {
         .filter((item) => item !== 'residents'));
     }
   }, [data]);
+
+  const renderTable = (filtered) => (
+    data.map((info) => (
+      <tr key={ info.name }>
+        <td>{ info.name }</td>
+        <td>{ info.rotation_period }</td>
+        <td>{ info.orbital_period }</td>
+        <td>{ info.diameter }</td>
+        <td>{ info.climate }</td>
+        <td>{ info.gravity }</td>
+        <td>{ info.terrain }</td>
+        <td>{ info.surface_water }</td>
+        <td>{ info.population }</td>
+        <td>{ info.films }</td>
+        <td>{ info.created }</td>
+        <td>{ info.edited }</td>
+        <td>{ info.url }</td>
+      </tr>
+    ))
+  );
 
   return (
     <table>
@@ -24,22 +44,10 @@ function Table() {
       </thead>
       <tbody>
         {
-          data.map((info) => (
-            <tr key={ info.name }>
-              <td>{ info.name }</td>
-              <td>{ info.rotation_period }</td>
-              <td>{ info.orbital_period }</td>
-              <td>{ info.diameter }</td>
-              <td>{ info.climate }</td>
-              <td>{ info.gravity }</td>
-              <td>{ info.terrain }</td>
-              <td>{ info.surface_water }</td>
-              <td>{ info.population }</td>
-              <td>{ info.films }</td>
-              <td>{ info.created }</td>
-              <td>{ info.edited }</td>
-              <td>{ info.url }</td>
-            </tr>
+          nameFilter.map((name) => (
+            name === ''
+              ? renderTable(data)
+              : renderTable(dataName)
           ))
         }
       </tbody>
